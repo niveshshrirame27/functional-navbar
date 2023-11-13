b3a2231c8c6bf7abff2de4aba200565d7b10a10e
niveshshrirame27 <niveshshrirame27@gmail.com>
2023-11-13 09:09:35 +0530

    topbar, navbar, div data, div content.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    const paragraphs = document.querySelectorAll('.navbar p');
+    const paragraphs = document.querySelectorAll('.topbar p');
     let index = 0;
   
     function updateParagraph() {
@@ -15,3 +17,25 @@ document.addEventListener("DOMContentLoaded", function() {
     } 
       setInterval(updateParagraph, 1500);
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const topbar = document.getElementById("topbar");
+    const navbar = document.getElementById("navbar");
+    let lastScrollTop = 0;
+
+    window.addEventListener("scroll", function() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scrolling down
+            topbar.style.top = `-${topbar.offsetHeight}px`;
+            navbar.style.top = "0";
+        } else {
+            // Scrolling up
+            topbar.style.top = "0";
+            navbar.style.top = `${topbar.offsetHeight}px`;
+        }
+
+        lastScrollTop = scrollTop;
+    });
+});
